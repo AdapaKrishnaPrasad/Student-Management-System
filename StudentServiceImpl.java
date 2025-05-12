82ef48cea81e55481ebe85598ae55941734659e8
Krishnaprasad Adapa <adapakrishnaprasad231@gmail.com>
2025-05-12 10:56:41 +0530

    Add files via upload

---
+++ StudentServiceImpl.java
@@ -0,0 +1,50 @@
+package com.sms.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sms.entity.Student;
+import com.sms.repository.StudentRepository;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        Optional<Student> student = studentRepository.findById(id);
+        return student.orElse(null);
+    }
+
+    @Override
+    public Student updateStudent(Long id, Student studentDetails) {
+        Student student = getStudentById(id);
+        if (student != null) {
+            student.setFirstName(studentDetails.getFirstName());
+            student.setLastName(studentDetails.getLastName());
+            student.setEmail(studentDetails.getEmail());
+            return studentRepository.save(student);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
